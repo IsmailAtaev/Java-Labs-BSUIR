f083f6b51e51bbb269f22c12c331b92461e1b722
IsmailAtaev <ataewisma@gmail.com>
2021-09-11 00:40:05 +0300

    lab2 add class MyThread

--- lab2/src/main/java/com/myThread/MyThread.java
+++ lab2/src/main/java/com/myThread/MyThread.java
@@ -7,6 +7,7 @@ package com.myThread;
  * 1. счетчик для рейсов
  * 2. грузовместимость потока или грузовика (1,5).(3.4).(5.1)
  * 3. timer for sleep() (30 min, 45 min, 40 min)
+ *
  * */
 
 public class MyThread implements Runnable {
