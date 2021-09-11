02f041cc48361044b2ee37b09039674c28ffe410
IsmailAtaev <ataewisma@gmail.com>
2021-09-11 23:45:25 +0300

    lab2 class MyThread add static Integer ton edit

--- lab2/src/main/java/com/myThread/Lab2Main.java
+++ lab2/src/main/java/com/myThread/Lab2Main.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lab2Main {
 
-    volatile static int ton = 10;
+    volatile static Integer ton = 10;
 
     public static void main(String[] args) throws InterruptedException {
         //AtomicInteger  ton = new AtomicInteger(86);
@@ -16,12 +16,16 @@ public class Lab2Main {
         MyThread thread3 = new MyThread("three", 100, 2, ton);
 
         thread1.start();
+        System.out.println(" i ton = " + ton);
         thread2.start();
+        System.out.println(" i ton = " + ton);
         thread3.start();
+        System.out.println(" i ton = " + ton);
 
         thread1.join();
         thread2.join();
         thread3.join();
+
 
         System.out.println("count ton = " + ton);
     }
