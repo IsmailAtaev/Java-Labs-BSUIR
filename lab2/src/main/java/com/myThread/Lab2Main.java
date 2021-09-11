91119bbecc179ed78813046cbfd2974e179a928a
IsmailAtaev <ataewisma@gmail.com>
2021-09-11 22:46:45 +0300

    lab2 class MyThread add static Integer ton

--- lab2/src/main/java/com/myThread/Lab2Main.java
+++ lab2/src/main/java/com/myThread/Lab2Main.java
@@ -7,13 +7,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lab2Main {
 
+    volatile static int ton = 10;
 
     public static void main(String[] args) throws InterruptedException {
-
-        AtomicInteger ton = new AtomicInteger(86);
+        //AtomicInteger  ton = new AtomicInteger(86);
         MyThread thread1 = new MyThread("one", 300, 2, ton);
-        MyThread thread2 = new MyThread("one", 200, 2, ton);
-        MyThread thread3 = new MyThread("one", 100, 2, ton);
+        MyThread thread2 = new MyThread("two", 200, 2, ton);
+        MyThread thread3 = new MyThread("three", 100, 2, ton);
 
         thread1.start();
         thread2.start();
@@ -24,7 +24,5 @@ public class Lab2Main {
         thread3.join();
 
         System.out.println("count ton = " + ton);
-        System.out.println(thread1.ton);
-
     }
 }
--- lab2/src/main/java/com/myThread/MyThread.java
+++ lab2/src/main/java/com/myThread/MyThread.java
@@ -18,37 +18,42 @@ import java.util.concurrent.atomic.AtomicInteger;
  * */
 
 public class MyThread extends Thread {
+
     private final Logger logger = LogManager.getLogger(MyThread.class.getName());
     private int countFlight = 0;
     private int timeFlight = 0; // millisecond
     private int volumeCargo = 0;
-    public AtomicInteger ton;
+    static public Integer ton;
     public Thread thread;
 
-    public MyThread(String name,int timeFlight, int volumeCargo,AtomicInteger ton) {
+    public MyThread(String name, int timeFlight, int volumeCargo, Integer ton) {
         thread = new Thread(name);
         this.timeFlight = timeFlight;
         this.volumeCargo = volumeCargo;
         this.ton = ton;
     }
 
-
     @Override
     public void run() {
-        while (true) {
+        this.sleeping();
+        System.out.println("Count of race = " + this.countFlight);
+        System.out.println("----------------------------" );
+    }
 
-
-            this.ton.addAndGet(-volumeCargo);
-
-
+    private synchronized void sleeping(){
+        while (ton > volumeCargo) {
+            System.out.println("Thread = " + this.thread.getName()+ " start ");
+            System.out.println("Ton after = " + this.ton);
+            //this.ton.addAndGet(-volumeCargo);
+            this.ton -= volumeCargo;
             try {
                 Thread.sleep(timeFlight);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             this.countFlight++;
-
+            System.out.println("Ton before = " + this.ton);
+            System.out.println("Thread " + this.thread.getName()+ " finish ");
         }
-       // System.out.println("count of race " + this.countFlight);
     }
 }
